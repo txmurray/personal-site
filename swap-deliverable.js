081fbcb6b92eb49187c74ef1eecbf9a17cdabc0e
Thomas X Murray <txm5665@gmail.com>
2019-12-16 22:09:51 -0500

    add basic javascript for text swap

---
+++ swap-deliverable.js
@@ -0,0 +1,73 @@
+const delayLoop = (fn, delay) => {
+    return (x, i) => {
+      setTimeout(() => {
+        fn(x);
+      }, i * delay);
+    };
+  };
+  
+const items = [
+"digital products",
+"logos",
+"websites",
+"icons",
+"apps",
+"pitch decks",
+"arcade cabinets",
+"dashboards",
+"digital ads",
+"animations",
+"print ads",
+"digital products",
+"ice sculptures",
+"emails",
+"neon signs",
+"experiences" 
+];
+  
+const output = document.querySelector("#output");
+  
+const display = s => output.innerText = s;
+
+
+items.forEach(
+  
+  delayLoop(display, 2000)
+  
+);
+
+
+// ---------------------------------------------------------
+// ---------------------------------------------------------
+
+
+// const items = [
+// "digital products",
+// "logos",
+// "websites",
+// "icons",
+// "apps",
+// "pitch decks",
+// "arcade cabinets",
+// "dashboards",
+// "digital ads",
+// "animations",
+// "print ads",
+// "digital products",
+// "ice sculptures",
+// "emails",
+// "neon signs",
+// "experiences" 
+// ];
+
+// const output = document.querySelector("#output");
+
+// function replaceText(i) {
+//   setTimeout(function() {
+//     output.innerText = items[i];
+//   }, 2000 * i);
+// }
+
+// for (let i = 0; i < items.length; i++) {
+//   replaceText(i);
+// };
